177eb22e710a561cbb68afe443180a4c4b4a239a
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-02-11 11:03:55 +0200

    Replaced jest dependency with vitest

---
+++ vitest.config.ts
@@ -0,0 +1,30 @@
+/*
+ *   MIT License
+ *
+ *   Copyright (c) 2024, Mattias Aabmets
+ *
+ *   The contents of this file are subject to the terms and conditions defined in the License.
+ *   You may not use, modify, or distribute this file except in compliance with the License.
+ *
+ *   SPDX-License-Identifier: MIT
+ */
+
+import path from "node:path";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+   test: {
+      include: ["vitests/**/*"],
+      server: {
+         deps: {
+            external: ["typescript"],
+         },
+      },
+   },
+   resolve: {
+      alias: {
+         "@src": path.resolve(__dirname, "./src"),
+         "@types": path.resolve(__dirname, "./types/internal.d.ts"),
+      },
+   },
+});
