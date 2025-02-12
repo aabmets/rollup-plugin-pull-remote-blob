78d2983e78980384db558201ec55160b26e42375
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-02-12 14:55:35 +0200

    Created constants module

---
+++ src/constants.ts
@@ -0,0 +1,26 @@
+/*
+ *   MIT License
+ *
+ *   Copyright (c) 2024, Mattias Aabmets
+ *
+ *   The contents of this file are subject to the terms and conditions defined in the License.
+ *   You may not use, modify, or distribute this file except in compliance with the License.
+ *
+ *   SPDX-License-Identifier: MIT
+ */
+
+import path from "node:path";
+import url from "node:url";
+import type * as t from "@types";
+
+const historyFileName = "historical_entries.json";
+
+const historyFilePath: string = (() => {
+   const filename = url.fileURLToPath(import.meta.url);
+   const dirname = path.dirname(filename);
+   return path.resolve(dirname, historyFileName);
+})();
+
+const defaultPluginConfig: t.PluginConfig = { verbose: true, blobs: [] };
+
+export default { historyFilePath, defaultPluginConfig };
