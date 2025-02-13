19f788c56af60367185c90a1748ce640969a262a
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-02-13 17:52:48 +0200

    Refactored history funcs into utils module

--- src/utils.ts
+++ src/utils.ts
@@ -15,6 +15,7 @@ import fsp from "node:fs/promises";
 import path from "node:path";
 import type * as t from "@types";
 import axios from "axios";
+import * as c from "./constants.js";
 
 function sortPathsByDepth(paths: string[], sep = path.sep) {
    return paths.sort((a: string, b: string) => {
@@ -24,24 +25,24 @@ function sortPathsByDepth(paths: string[], sep = path.sep) {
    });
 }
 
-function digestString(data: string, outputSize = 64): string {
+function digestString(data: string): string {
    const hash = crypto.createHash("sha256");
    const digest = hash.update(data).digest("hex");
-   return digest.substring(0, outputSize);
+   return digest.substring(0, 64);
 }
 
-function getDestDetails(srcObj: t.RemoteBlobOption | t.HistoryFileEntry): t.DestDetails {
-   if (path.extname(srcObj.dest) !== "" && "decompress" in srcObj && srcObj?.decompress) {
-      throw new Error(`Destination must be a directory when decompressing: '${srcObj.dest}'`);
-   }
-   const destPath = path.resolve(srcObj.dest);
+function getDestDetails(option: t.RemoteBlobOption): t.DestDetails {
+   const destPath = path.resolve(option.dest);
    const isFile = path.extname(destPath) !== "";
+   if (isFile && option?.decompress) {
+      throw new Error(`Destination must be a directory when decompressing: '${option.dest}'`);
+   }
    const dirPath = isFile ? path.dirname(destPath) : destPath;
-   const fileName = isFile ? path.basename(destPath) : path.basename(srcObj.url);
+   const fileName = path.basename(isFile ? destPath : option.url);
    const filePath = path.join(dirPath, fileName);
    const fileExists = fs.existsSync(filePath);
    const dirExists = fs.existsSync(dirPath);
-   return { fileExists, filePath, dirExists, dirPath, isFile };
+   return { fileExists, filePath, dirExists, dirPath };
 }
 
 async function downloadFile(option: t.RemoteBlobOption, dest: t.DestDetails): Promise<void> {
@@ -50,4 +51,32 @@ async function downloadFile(option: t.RemoteBlobOption, dest: t.DestDetails): Pr
    await fsp.writeFile(dest.filePath, response.data);
 }
 
-export default { sortPathsByDepth, digestString, getDestDetails, downloadFile };
+function readHistoryFile(): t.HistoryFileContents {
+   try {
+      if (fs.existsSync(c.historyFilePath)) {
+         const historyData = fs.readFileSync(c.historyFilePath, "utf8");
+         return historyData ? JSON.parse(historyData) : {};
+      }
+   } catch {
+      return {};
+   }
+   return {};
+}
+
+function writeHistoryFile(entries: t.HistoryFileEntry[]): void {
+   const data: t.HistoryFileContents = {};
+   entries.forEach((entry) => {
+      data[entry.optionsDigest] = entry;
+   });
+   fs.mkdirSync(path.dirname(c.historyFilePath), { recursive: true });
+   fs.writeFileSync(c.historyFilePath, JSON.stringify(data, null, 2));
+}
+
+export default {
+   sortPathsByDepth,
+   digestString,
+   getDestDetails,
+   downloadFile,
+   readHistoryFile,
+   writeHistoryFile,
+};
